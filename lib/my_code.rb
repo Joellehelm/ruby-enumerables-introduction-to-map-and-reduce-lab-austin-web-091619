02414d3ec39b96d02c03d213fb00e7514a2c87af
Joelle Helm <joellenhelm@gmail.com>
2019-08-27 17:37:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,30 +1,76 @@
 # My Code here....
-def map_to_negativize(source_array)
-  source_array.map { |num| num * -1 }
-end
-
-def map_to_no_change(source_array)
-  source_array.map! { |ele| }
-end
-
-def map_to_double(source_array)
-  source_array.map { |num| num * 2 }
-end
-
-def map_to_square(source_array)
-  source_array.map { |num| num * num }
-end
-
-
-
-def reduce_to_total(source_array, starting_point = 0)
-   source_array.reduce(starting_point, :+)
-end
-
-def reduce_to_all_true(source_array)
-  source_array.all?
+# def map_to_negativize(source_array)
+#   source_array.map { |num| num * -1 }
+# end
+  def map_to_negativize(source_array)
+    i = 0
+    new = []
+    while i < source_array.length
+      new << source_array[i] * -1
+      i += 1
+    end
+    return new
   end
+#-----------------------------------------------
 
-def reduce_to_any_true(source_array)
-  source_array.any?
-end
+# def map_to_no_change(source_array)
+#   source_array.map! { |ele| }
+# end
+  def map_to_no_change(source_array)
+    i = 0
+    new = []
+    while i < source_array.length
+      new << source_array[i]
+      i += 1
+    end
+    return new
+  end
+#-----------------------------------------------
+
+# def map_to_double(source_array)
+#   source_array.map { |num| num * 2 }
+# end
+  def map_to_double(source_array)
+    new = []
+    i = 0
+    while i < source_array.length
+      new << source_array[i] * 2
+      i += 1
+    end
+    return new
+  end
+#-----------------------------------------------
+
+# def map_to_square(source_array)
+#   source_array.map { |num| num * num }
+# end
+  def map_to_square(source_array)
+    i = 0
+    new = []
+    while i < source_array.length
+      new << source_array[i] * source_array[i]
+      i += 1
+    end
+
+  end
+#-----------------------------------------------
+#
+#
+# def reduce_to_total(source_array, starting_point = 0)
+#    source_array.reduce(starting_point, :+)
+# end
+  def reduce_to_total(source_array, starting_point = 0)
+    i = 0
+    new = starting_point
+    while i < source_array.length
+      new += source_array[i]
+    end
+    return new
+  end
+# def reduce_to_all_true(source_array)
+#   source_array.all?
+#   end
+#
+# def reduce_to_any_true(source_array)
+#   source_array.any?
+# end
